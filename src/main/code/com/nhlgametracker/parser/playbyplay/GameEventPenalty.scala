464d285ac04721f54302dab24aacb255be468f2f
Harri Lehtola <peruukki@hotmail.com>
2013-02-02 06:23:43 +0200

    Parse penalty duration

--- src/main/code/com/nhlgametracker/parser/playbyplay/GameEventPenalty.scala
+++ src/main/code/com/nhlgametracker/parser/playbyplay/GameEventPenalty.scala
@@ -6,14 +6,14 @@ import xml.NodeSeq
 class GameEventPenalty(columns: NodeSeq, description: String)
   extends GameEvent(columns: NodeSeq)
 {
-  val (team, taker, reason) = parseDescription(description)
+  val (team, taker, reason, duration) = parseDescription(description)
 
   private def parseDescription(description: String) = {
     val pattern = new Regex(PATTERN_TEAM + """(""" + PATTERN_PLAYER + """|TEAM)""" +
-                            """\u00a0(.+min\))""", "team", "taker", "reason")
+                            """\u00a0(.+\((\d+)\smin\))""", "team", "taker", "reason", "duration")
     pattern.findFirstMatchIn(description) match {
       case Some(penaltyMatch) => (penaltyMatch.group("team"), trim(penaltyMatch.group("taker")),
-                                  trim(penaltyMatch.group("reason")))
+                                  trim(penaltyMatch.group("reason")), penaltyMatch.group("duration"))
       case None => throw new RuntimeException("No match in '%s'".format(description))
     }
   }
@@ -23,6 +23,7 @@ class GameEventPenalty(columns: NodeSeq, description: String)
     appendValue(builder, "team", team)
     appendValue(builder, "taker", taker)
     appendValue(builder, "reason", reason)
+    appendValue(builder, "duration", duration)
     super.finishJson(builder)
   }
 }
