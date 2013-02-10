ad5e2f836664263d6a92c176510ebc0cd1fe3b03
Harri Lehtola <peruukki@hotmail.com>
2013-02-10 22:01:45 +0200

    Fix team name parsing with Scala 2.10.0

--- src/main/code/com/nhlreplay/parser/playbyplay/Team.scala
+++ src/main/code/com/nhlreplay/parser/playbyplay/Team.scala
@@ -7,7 +7,7 @@ class Team(columns: NodeSeq)
   val teamName = getTeamName(columns)
 
   private def getTeamName(columns: NodeSeq) = {
-    val pattern = """\s+(.+)<br>""".r
+    val pattern = """>(.+)<br/>""".r
     val content = ((columns \ "tbody" \ "tr")(2) \ "td").mkString
     pattern.findFirstMatchIn(content) match {
       case Some(teamMatch) => teamMatch.group(1)
