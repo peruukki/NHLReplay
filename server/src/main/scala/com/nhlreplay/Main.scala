c3326696c607d9fcb5e30cc8f5ae422ae551bd94
Harri Lehtola <peruukki@hotmail.com>
2013-03-05 22:03:21 +0200

    Isolate play-by-play report parsing

--- server/src/main/scala/com/nhlreplay/Main.scala
+++ server/src/main/scala/com/nhlreplay/Main.scala
@@ -6,10 +6,12 @@ import parser.playbyplay.GameEventParser
 object Main
 {
   def main(args: Array[String]) {
-    val converter = new XhtmlConverter()
-    val xhtmlFile = converter.convertHtml("src/test/resources/playbyplay/PL020160.HTM")
-    val parser = new GameEventParser()
-    val gameInfo = parser.parse(xhtmlFile)
+    parsePlayByPlay()
+  }
+
+  private def parsePlayByPlay() {
+    val xhtmlFile = new XhtmlConverter().convertHtml("src/test/resources/playbyplay/PL020160.HTM")
+    val gameInfo = new GameEventParser().parse(xhtmlFile)
     gameInfo.writeToJsonpFile("../client/src/main/jsonp/data.jsonp")
   }
 }
