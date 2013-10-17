4c82093c6708f6647bc7ff1755a5245ceca83807
Harri Lehtola <peruukki@hotmail.com>
2013-10-17 22:45:56 +0300

    Use string interpolation when generating jsonp

--- server/src/main/scala/com/nhlreplay/parser/playbyplay/GameInfo.scala
+++ server/src/main/scala/com/nhlreplay/parser/playbyplay/GameInfo.scala
@@ -16,17 +16,17 @@ case class GameInfo(teams: GameTeams, events: GameEvents)
 
   def toJsonp: Seq[String] = {
     val jsonp = new ListBuffer[String]
-    jsonp += """function getJsonData() {
-               |  var %s = '{ ';
-               |""".stripMargin.format(dataVariable)
+    jsonp += s"""function getJsonData() {
+               |  var $dataVariable = '{ ';
+               |""".stripMargin
     jsonp += getJsonpEvents
-    jsonp += "\n  %s += ',';\n".format(dataVariable)
+    jsonp += s"\n  $dataVariable += ',';\n"
     jsonp += getJsonpTeams
-    jsonp += """
-               |  %s += '}'
+    jsonp += s"""
+               |  $dataVariable += '}'
                |
-               |  return %s;
-               |}""".stripMargin.format(dataVariable, dataVariable)
+               |  return $dataVariable;
+               |}""".stripMargin
     jsonp.toSeq
   }
 
@@ -35,9 +35,9 @@ case class GameInfo(teams: GameTeams, events: GameEvents)
 
   private def getJsonp(name: String, values: Seq[HasJson]) = {
     val builder = new StringBuilder()
-    builder.append("""  %s += '"%s": [' +
-                     |""".stripMargin.format(dataVariable, name))
-    values foreach { v => builder.append("    '").append(v.toJson).append(",' +\n") }
+    builder.append(s"""  $dataVariable += '"$name": [' +
+                     |""".stripMargin)
+    values.foreach(value => builder.append("    '").append(value.toJson).append(",' +\n"))
     builder.deleteCharAt(builder.lastIndexOf(","))
     builder.append("  ']';")
     builder.toString()
