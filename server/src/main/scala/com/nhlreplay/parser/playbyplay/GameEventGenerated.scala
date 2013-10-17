b367581c8ce28aae83273cec0b8d4f929c250662
Harri Lehtola <peruukki@hotmail.com>
2013-10-17 21:27:44 +0300

    Remove unnecessary curly brackets from empty class

--- server/src/main/scala/com/nhlreplay/parser/playbyplay/GameEventGenerated.scala
+++ server/src/main/scala/com/nhlreplay/parser/playbyplay/GameEventGenerated.scala
@@ -4,5 +4,3 @@ import xml.NodeSeq
 
 abstract class GameEventGenerated(columns: NodeSeq, override val eventType: String)
   extends GameEvent(columns, generateGoalAttempt = false, ignore = false)
-{
-}
