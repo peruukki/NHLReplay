74f2ff80b545236025544668cf0b4a3cef8d2222
Harri Lehtola <peruukki@hotmail.com>
2013-10-20 15:57:38 +0300

    Add default Play dependencies

--- server/project/NHLReplayBuild.scala
+++ server/project/NHLReplayBuild.scala
@@ -5,15 +5,13 @@ import org.scalastyle.sbt._
 import play.Project._
 
 object NHLReplayBuild extends Build {
-  import Dependency._
-  import Resolvers._
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization  := "com.nhlreplay",
     version       := "0.1",
     scalaVersion  := "2.10.2",
     scalacOptions := Seq("-deprecation", "-feature", "-unchecked"),
-    resolvers     := allResolvers
+    resolvers     := Resolvers.allResolvers
   )
   lazy val jacocoSettings = jacoco.settings
   lazy val scalastyleSettings = ScalastylePlugin.Settings
@@ -22,7 +20,8 @@ object NHLReplayBuild extends Build {
   lazy val root = play.Project(
     name = "NHLReplay",
     settings = buildSettings ++ jacocoSettings ++ scalastyleSettings ++ playSettings ++ Seq(
-      libraryDependencies ++= Seq(json4sNative, logback, scalaLogging, scalaTest)
+      libraryDependencies ++=
+        Seq(Dependency.json4sNative, Dependency.logback, Dependency.scalaLogging, Dependency.scalaTest) ++ Dependency.play
     )
   )
 
@@ -31,6 +30,7 @@ object NHLReplayBuild extends Build {
     val logback = "ch.qos.logback" % "logback-classic" % "1.0.13"
     val scalaLogging = "com.typesafe" %% "scalalogging-slf4j" % "1.0.1"
     val scalaTest = "org.scalatest" % "scalatest_2.10" % "1.9.2" % "test"
+    val play = Seq(jdbc, anorm, cache)
   }
 
   object Resolvers {
